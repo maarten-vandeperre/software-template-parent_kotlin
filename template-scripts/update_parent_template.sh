176e8300bf0f46599058e8a265bc1a8eeb24b92e
Maarten Vandeperre @ Red Hat <mvandepe@redhat.com>
2024-12-14 22:00:30 +0100

    git fix (or try to fix) the update parent template script

--- template-scripts/update_parent_template.sh
+++ template-scripts/update_parent_template.sh
@@ -17,48 +17,61 @@ CHILD_DIR="_submodules/software-template-parent"
 
 # Function to extract custom content from a file
 extract_custom_content() {
-    prefix = $1
-    awk "/\/\/ #### $prefix-start ####/,/\/\/ #### $prefix-stop ####/" "$2"
+    local prefix=$1
+    local file=$2
+    awk "/\/\/ #### ${prefix}-start ####/{flag=1; next} /\/\/ #### ${prefix}-stop ####/{flag=0} flag" "$file"
 }
 
-copy_custom_code_to_git_module(){
-  prefix=$1
-  echo "start copy_custom_code_to_git_module"
-  # Process all .gradle.kts files in the child directory
-  find "$CHILD_DIR" -type f -name "*.gradle.kts" | while read -r CHILD_FILE; do
-      # Determine the corresponding parent file
-      RELATIVE_PATH="${CHILD_FILE#$CHILD_DIR/}"
-      PARENT_FILE="$PARENT_DIR/$RELATIVE_PATH"
+copy_custom_code_to_git_module() {
+    local prefix=$1
+    echo "Start copying custom blocks with prefix: $prefix"
 
-      # Check if the parent file exists
-      if [ -f "$PARENT_FILE" ]; then
-          # Extract custom content from the child file into a temporary file
-          TEMP_CONTENT_FILE=$(mktemp)
-          extract_custom_content "$prefix" "$CHILD_FILE" > "$TEMP_CONTENT_FILE"
+    # Find and process all .gradle.kts files in the child directory
+    find "$CHILD_DIR" -type f -name "*.gradle.kts" | while read -r CHILD_FILE; do
+        # Determine the corresponding parent file
+        RELATIVE_PATH="${CHILD_FILE#$CHILD_DIR/}"
+        PARENT_FILE="$PARENT_DIR/$RELATIVE_PATH"
 
-          # Update the parent file with the custom content
-          awk -v temp_file="$TEMP_CONTENT_FILE" '
-              BEGIN { in_custom = 0 }
-              {
-                  if ($0 ~ /\/\/ #### $prefix-start ####/) {
-                      in_custom = 1;
-                      print;  # Print the start marker
-                      while ((getline line < temp_file) > 0) print line;  # Inject custom content
-                      next;
-                  }
-                  if ($0 ~ /\/\/ #### $prefix-stop ####/) in_custom = 0;
-                  if (!in_custom) print;
-              }
-          ' "$PARENT_FILE" > "${PARENT_FILE}.tmp"
+        # Check if the parent file exists
+        if [ -f "$PARENT_FILE" ]; then
+            # Extract custom content from the child file into a temporary file
+            TEMP_CONTENT_FILE=$(mktemp)
+            extract_custom_content "$prefix" "$CHILD_FILE" > "$TEMP_CONTENT_FILE"
 
-          # Replace the parent file with the updated content
-          mv "${PARENT_FILE}.tmp" "$PARENT_FILE"
+            # Update the parent file with the custom content
+            awk -v temp_file="$TEMP_CONTENT_FILE" -v prefix="$prefix" '
+                BEGIN { in_custom = 0 }
+                {
+                    if ($0 ~ "\\/\\/ #### " prefix "-start ####") {
+                        in_custom = 1;
+                        print;  # Print the start marker
+                        while ((getline line < temp_file) > 0) print line;  # Inject custom content
+                        next;
+                    }
+                    if ($0 ~ "\\/\\/ #### " prefix "-stop ####") in_custom = 0;
+                    if (!in_custom) print;
+                }
+            ' "$PARENT_FILE" > "${PARENT_FILE}.tmp"
 
-          # Remove the temporary file
-          rm -f "$TEMP_CONTENT_FILE"
-      fi
-  done
-  echo "end copy_custom_code_to_git_module"
+            # Replace the parent file with the updated content
+            mv "${PARENT_FILE}.tmp" "$PARENT_FILE"
+
+            # Remove the temporary file
+            rm -f "$TEMP_CONTENT_FILE"
+        else
+            echo "Parent file does not exist for: $CHILD_FILE (skipping)"
+        fi
+    done
+
+    echo "Finished copying custom blocks with prefix: $prefix"
+}
+
+# Main logic to process multiple prefixes
+copy_custom_blocks_to_parent() {
+    prefixes=("custom-dependencies" "custom-project-metadata" "custom-code")
+    for prefix in "${prefixes[@]}"; do
+        copy_custom_code_to_git_module "$prefix"
+    done
 }
 
 delete_child_folder() {
@@ -83,8 +96,7 @@ reset_git_module() {
 
 ####### script
 
-copy_custom_code_to_git_module "custom-code"
-copy_custom_code_to_git_module "custom-project-metadata"
+copy_custom_blocks_to_parent
 delete_child_folder
 copy_git_module_code_to_child
 reset_git_module
