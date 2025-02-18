b634b4fd4f3e70156a0771f2de75409e672b00e6
Maarten Vandeperre @ Red Hat <mvandepe@redhat.com>
2025-02-18 13:06:52 +0100

    add open liberty resource

--- application/configuration/open-liberty/monolith/src/main/java/com/demo/rest/RestApplication.java
+++ application/configuration/open-liberty/monolith/src/main/java/com/demo/rest/RestApplication.java
@@ -1,16 +1,8 @@
 package com.demo.rest;
 
 import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Application;
-import jakarta.ws.rs.core.MediaType;
 
 @ApplicationPath("/api")
 public class RestApplication extends Application {
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Open Liberty";
-    }
 }
